3e44c987b37acfd9640fbe21f560dba41e88807c
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-22 10:48:26 +0200

    added schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,17 @@ import { SeasonsComponent } from './seasons/seasons.component';
 import { ListComponent } from './seasons/list/list.component';
 import { DetailComponent } from './seasons/detail/detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SeasonsComponent, ListComponent, DetailComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomeComponent, SeasonsComponent, ListComponent, DetailComponent, NavigationComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
