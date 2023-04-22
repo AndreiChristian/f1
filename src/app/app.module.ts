bd8668e6e443d0976bfceaa0d4e9ce81d0067135
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-22 08:51:16 +0200

    basic setup for testing the api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SeasonsComponent } from './seasons/seasons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SeasonsComponent
   ],
   imports: [
     BrowserModule,
